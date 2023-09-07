b6e742fa26b58a70d264fec1f36adc7ff23d472f
Tavish Bhardwaj <bhardwajtavishofficial@gmail.com>
2023-09-07 15:08:21 +0530

    Image paths modified

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "src\images\mlsa3.jpg";
+import image from "src/images/mlsa3.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
